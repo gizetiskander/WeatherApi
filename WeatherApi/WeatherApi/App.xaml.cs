21058411ca7881245989870b8a38075582f172a9
Гизетдинов Искандер <gizetiskander@gmail.com>
2022-05-26 15:28:59 +0300

    Добавьте файлы проекта.

---
+++ WeatherApi/WeatherApi/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using WeatherApi.Views;
+
+namespace WeatherApi
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new WeatheApiPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
